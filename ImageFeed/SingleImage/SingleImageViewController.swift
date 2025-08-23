b899c67e9dfa91c7b6d439a103e9ea0ea01dfd52
Alla Mozharova <alik19.09@mail.ru>
2025-08-24 01:13:36 +0300

    Реализован показ соответствующих изображений при нажатии на строку

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -2,6 +2,17 @@ import Foundation
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
     @IBOutlet private weak var imageView: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
