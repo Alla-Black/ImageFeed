9dbb14fb4711e31ab82742d73c967c01c2ef8a0f
Alla Mozharova <alik19.09@mail.ru>
2025-09-01 18:33:44 +0300

    Создано нэймспэйс enum Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "7KYXtrTofya9kTzJtmTqARXUgvOulkBdpqmF1RATbMQ"
+    static let secretKey = "Pcm22qIw43UMY_lysyBpLVhngdPJW_0fFj8d8SKBXDI"
+    static let redirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
