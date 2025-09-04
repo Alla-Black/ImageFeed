515daea000aae8b7fe3d5605ddef11f03a0419c2
Alla Mozharova <alik19.09@mail.ru>
2025-09-04 20:11:27 +0300

    написан запрос для получения кода авторизации, выполнен через WebView. Сформирован запрос и получение уведомлений о действиях пользователя через делегат

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -15,10 +15,34 @@ final class AuthViewController: UIViewController {
         configureBackButton ()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController
+            else {
+                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+                return
+                }
+                webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
     private func configureBackButton () {
         navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate { func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+    print("Получили код авторизации: \(code)")
+    
+    //TODO: process code
+}
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        print("Пользователь отменил авторизацию")
+        dismiss(animated: true)
     }
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -2,16 +2,21 @@ import Foundation
 import UIKit
 import WebKit
 
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
 final class WebViewViewController: UIViewController {
     @IBOutlet private weak var webView: WKWebView!
     
+    weak var delegate: WebViewViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        
         loadAuthView()
-    }
-    
-    enum WebViewConstants {
-        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
     }
     
     private func loadAuthView() {
@@ -32,6 +37,37 @@ final class WebViewViewController: UIViewController {
         
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+                urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
     }
 }
 
