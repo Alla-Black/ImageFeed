27d0d781e0672d15a10cfd2d97fe3ddbfd840842
Alla Mozharova <alik19.09@mail.ru>
2025-10-13 21:28:48 +0300

    chore: удалить временный тест ImagesListServiceTests

--- ImageFeedTests/ImagesListServiceTests.swift
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -1,24 +1,3 @@
 import Testing
 @testable import ImageFeed
 import XCTest
-
-final class ImagesListServiceTests: XCTestCase {
-    func testFetchPhotos() {
-        let service = ImagesListService()
-        
-        let expectation = self.expectation(description: "Wait for Notification")
-        NotificationCenter.default.addObserver(
-            forName: ImagesListService.didChangeNotification,
-            object: service,
-            queue: .main) { _ in
-                expectation.fulfill()
-            }
-        
-        let token = OAuth2TokenStorage.shared.token
-        XCTAssertNotNil(token, "В тестовой среде нет токена — запрос не стартует")
-        service.fetchPhotosNextPage()
-        wait(for: [expectation], timeout: 10)
-        
-        XCTAssertEqual(service.photos.count, 10)
-    }
-}
