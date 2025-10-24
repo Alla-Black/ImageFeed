13ecd39dc708aef1484c0e13135dc2888603f77c
Alla Mozharova <alik19.09@mail.ru>
2025-10-24 16:02:46 +0300

    test(WebViewViewController): добавить тест на вызов метода viewDidLoad презентера вьюконтроллером

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,40 @@
+import Testing
+import ImageFeed
+import Foundation
+@testable import ImageFeed
+import XCTest
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+}
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
