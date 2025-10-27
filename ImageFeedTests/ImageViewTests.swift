fd46ca0add5c6a880e477ebc05fe8ed6ad0a9d88
Alla Mozharova <alik19.09@mail.ru>
2025-10-27 18:58:13 +0300

    test(ImagesListViewController): проверить, что вью контроллер корректно делегирует метод willDisplay презентеру

--- ImageFeedTests/ImageViewTests.swift
+++ ImageFeedTests/ImageViewTests.swift
@@ -18,18 +18,39 @@ final class ImageViewTests: XCTestCase {
         //then
         XCTAssertTrue(presenterSpy.viewDidLoadCalled)
     }
+    
+    func testViewControllerForwardsWillDisplayToPresenter() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let sut = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        ) as! ImagesListViewController
+        
+        let presenterSpy = ImagesListPresenterSpy()
+        sut.configure(presenterSpy)
+        
+        //when
+        sut.tableView(UITableView(), willDisplay: UITableViewCell(), forRowAt: IndexPath(row: 3, section: 0))
+        
+        //then
+        XCTAssertTrue(presenterSpy.willDisplayCalled)
+        XCTAssertEqual(presenterSpy.receivedIndexPath, IndexPath(row:3, section: 0))
+    }
 }
 
 class ImagesListPresenterSpy: ImagesListPresenterProtocol {
     var view: ImagesListViewControllerProtocol?
     var viewDidLoadCalled: Bool = false
+    var willDisplayCalled = false
+    var receivedIndexPath: IndexPath?
     
     func viewDidLoad() {
         viewDidLoadCalled = true
     }
     
     func willDisplayCell(at indexPath: IndexPath) {
-        
+        willDisplayCalled = true
+        receivedIndexPath = indexPath
     }
     
     func didTapLike(at indexPath: IndexPath) {
